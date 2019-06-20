73fd3b043da2a71281198e3ec6c12040f124cbb3
Cameron Grant <cameron.emil.grant@gmail.com>
2019-06-20 16:23:27 -0700

    Code Cleanup

--- Arduino/ConsoleCode/ConsoleCode.ino
+++ Arduino/ConsoleCode/ConsoleCode.ino
@@ -21,16 +21,7 @@ bool firstBoot = true;
 unsigned char dataByte1, dataByte2, dataByte3, dataByte4;
 unsigned char incomingByte;
 unsigned char byteIdentifier;
-char* data;
 volatile char command;
-char numBytesRead;
-char numBytesAvailable;
-volatile bool handleConsoleData = false;
-char state = 0;
-char nextState = 0;
-unsigned char readByte;
-bool dataOrdered = true;
-bool breakout = false;
 
 Beam64 beam64(CONTROLLER_DATA_PIN, false);
 
@@ -61,21 +52,21 @@ void loop()
 	}
 	
 	while (1) {
+		// Check for serial data from bluetooth receiver
 		if (*_ucsra & (1 << RXCn)) {
 			incomingByte = *_udrn;
 		}
 
-		//might need to turn off interupts here?
 		switch (incomingByte & 0xF0) {
 		case 0x90 :
 			dataByte1 = (incomingByte << 4) | (dataByte1 & 0x0F);
 			break;
 
-		case 0x80:
+		case 0x80 :
 			dataByte1 = (incomingByte & 0x0F) | (dataByte1 & 0xF0);
 			break;
 
-		case 0x70:
+		case 0x70 :
 			dataByte2 = (incomingByte << 4) | (dataByte2 & 0x0F);
 			break;
 
@@ -87,35 +78,13 @@ void loop()
 			if (incomingByte & 0x01) {
 				dataByte4 = incomingByte;
 			}
-
 			else {
 				dataByte3 = incomingByte;
 			}
 			break;
 
 		}
-
-		//PORTB |= 2;
-		//PORTB &= 0;
 	}
-	
-
-	//// check for data on serial
-	
-	//if (*_ucsra & (1 << RXCn)) {
-	//	digitalWrite(TEST_PIN, HIGH);
-	//	//dataByte3 = *_udrn;
-	//	digitalWrite(TEST_PIN, LOW);
-	//}
-
-	//while (1) {
-	//	digitalWrite(TEST_PIN, HIGH);
-	//	digitalWrite(TEST_PIN, LOW);
-	//	if (handleConsoleData) {
-	//		break;
-	//	}
-	//}
-
 }
 
 void handleConsole() {
@@ -123,13 +92,6 @@ void handleConsole() {
 
 	//manually pin interrupt triggered by data being sent
 	*_eifr = 2;
-}
-
-unsigned char USART_Receive(void) {
-	/* Wait for data to be received */
-	while (!(*_ucsra & (1 << RXCn)));
-	/* Get and return received data from buffer */
-	return *_udrn;
 }
 
 void waitForConsoleCMD() {
@@ -159,83 +121,3 @@ void waitForConsoleCMD() {
 	}
 }
 
-int homemadeDelay() {
-	int count = 0;
-	int anumber;
-
-	while (count < 31000) {
-		count++;
-		anumber = count + count;
-	}
-
-	return count;
-}
-
-
-
-
-//if (Serial.available() > 0) {
-//	breakout = false;
-//	while (dataOrdered == true) {
-//		int loopCounter = 0;
-//		while (Serial.available() == 0) {
-//			loopCounter++;
-//			if (loopCounter > 255) {
-//				breakout = true;
-//				break;
-//			}	
-//		}
-//		if (breakout) break;
-
-//		readByte = Serial.read();
-//		//Serial.write(readByte);
-//		
-
-//		if (state == 0) {
-//			if (readByte == 0xC0) {
-//				nextState = 1;
-//			}
-//			else {
-//				dataOrdered = false;
-//				nextState = 0;
-//			}
-//		}
-
-//		if (state == 1) {
-//			if (readByte == 0x99) {
-//				nextState = 2;
-
-//			}
-//			else {
-//				dataOrdered = false;
-//				nextState = 0;
-//			}
-//		}
-
-//		if (state == 2) {
-//			nextState = 3;
-//			dataByte3 = readByte;
-//		}
-
-//		if (state == 3) {
-//			nextState = 10;
-//			dataByte4 = readByte;
-//		}
-
-//		if (state == 10) {
-//			nextState = 15;
-//			dataByte1 = readByte;
-//		}
-
-//		if (state == 15) {
-//			dataOrdered = false;
-//			nextState = 0;
-//			dataByte2 = readByte;
-//		}
-
-//		state = nextState;
-//	}
-
-//}
-//
-//dataOrdered = true;
